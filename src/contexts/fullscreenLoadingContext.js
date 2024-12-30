43d8c8a80b457c9a4be549a712f61bd1d6375e67
lahkokaku <jose.satrya@gmail.com>
2024-12-30 23:12:17 +0700

    chore: update tailwind content path

--- src/contexts/fullscreenLoadingContext.js
+++ src/contexts/fullscreenLoadingContext.js
@@ -19,15 +19,12 @@ export const FullscreenLoadingContextProvider = ({ children }) => {
       {
         isFullScreenLoading ? (
           <div
-            className="absolute top-0 left-0 z-10 w-full h-full flex justify-center items-center"
-            style={
-              { backgroundColor: 'rgba(1,1,1,0.6)' }
-            }
+            className="absolute top-0 left-0 z-10 w-full h-full flex justify-center items-center bg-neutral-900/50"
           >
             <div
               className="bg-neutral-50 rounded-lg drop-shadow-md p-5 flex justify-center"
             >
-              <Spinner className="w-1/3" />
+              <Spinner className="w-1/3 h-1/3" />
             </div>
           </div>
         ) : null
