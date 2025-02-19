7f6c4efd63f3f74cd9ba42af8b12be1cc4bcdb91
lahkokaku <jose.satrya@gmail.com>
2025-02-19 23:02:51 +0700

    bugfix: fix logic in opening call page

--- src/app/contact/components/ConversationHeader.jsx
+++ src/app/contact/components/ConversationHeader.jsx
@@ -1,10 +1,10 @@
-import Image from "next/image"
-import { FaArrowLeft, FaPhone } from "react-icons/fa6"
-import { useConversationContext } from "@/contexts/conversationContext"
-import { useRouter } from "next/navigation"
-import { toast } from "react-toastify"
-import { validateRoomData } from "@/app/call/[id]/actions"
-import { profileConstructor } from "@/utils/conversationHelper"
+import Image from 'next/image'
+import { FaArrowLeft, FaPhone } from 'react-icons/fa6'
+import { useConversationContext } from '@/contexts/conversationContext'
+import { useRouter } from 'next/navigation'
+import { toast } from 'react-toastify'
+import { validateRoomData } from '@/app/call/[id]/actions'
+import { profileConstructor } from '@/utils/conversationHelper'
 
 const ConversationHeader = ({ userId }) => {
   const router = useRouter()
@@ -27,7 +27,7 @@ const ConversationHeader = ({ userId }) => {
   }
 
   const handleProfileClick = () => {
-    if (type === "peer") {
+    if (type === 'peer') {
       router.push(`/contact/peer?email=${peer.email}`)
       return
     }
@@ -35,25 +35,25 @@ const ConversationHeader = ({ userId }) => {
   }
 
   const initiateCall = () => {
-    setIsCallOngoing(true)
-    if (!isCallOngoing) {
-      let popup = null
-      popup = window.open(
-        `/call/${activeConversation._id}?type=${type}`,
-        "_blank",
-        "menubar=no,status=no,width=1280"
-      )
-      popup.onpagehide = (e) => {
-        console.log(e)
-        if (e?.timeStamp < 1000) {
-          return
-        }
-        validateRoomData(type, activeConversation._id)
-        setIsCallOngoing(false)
-      }
+    if (isCallOngoing) {
+      toast.error('You must leave the other call first.')
       return
     }
-    toast.error("You must leave the other call first.")
+    setIsCallOngoing(true)
+    let popup = null
+    popup = window.open(
+      `/call/${activeConversation._id}?type=${type}`,
+      '_blank',
+      'menubar=no,status=no,width=1280'
+    )
+    popup.onpagehide = (e) => {
+      console.log(e)
+      if (e?.timeStamp < 1000) {
+        return
+      }
+      validateRoomData(type, activeConversation._id)
+      setIsCallOngoing(false)
+    }
   }
 
   return (
@@ -66,7 +66,7 @@ const ConversationHeader = ({ userId }) => {
           <FaArrowLeft />
         </div>
         <Image
-          src={displayedPicture || "/images/default-profile-picture.webp"}
+          src={displayedPicture || '/images/default-profile-picture.webp'}
           height={48}
           width={48}
           alt="Profile picture"
@@ -82,8 +82,8 @@ const ConversationHeader = ({ userId }) => {
       </div>
       <div
         className={
-          "flex items-center gap-5 font-semibold text-2xl " +
-          (onCall.length > 0 ? "text-primary animate-bounce" : "text-secondary")
+          'flex items-center gap-5 font-semibold text-2xl ' +
+          (onCall.length > 0 ? 'text-primary animate-bounce' : 'text-secondary')
         }
       >
         <FaPhone className="cursor-pointer" onClick={initiateCall} />
